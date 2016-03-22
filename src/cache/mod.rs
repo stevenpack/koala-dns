fe669465ada7beccdc024cede38edecee98f331c
stevenpack <steven_john_pack@yahoo.com.au>
2016-03-22 16:27:19 -0700

    test fixes

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -35,29 +35,34 @@ impl Cache  {
         self.map.get(key)
     }
 
-    // pub fn contains(&self, key: &CacheKey) -> bool {
-    //     self.map.contains_key(&key)
-    // }
+    pub fn contains(&self, key: &CacheKey) -> bool {
+        self.map.contains_key(&key)
+    }
 
-    // pub fn len(&self) -> usize {
-    //     self.map.len()
-    // }
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
 
     pub fn remove_expired(&mut self) -> usize {
-        let now = SteadyTime::now();
-        let mut count = 0;
-        for cache_expiry in self.keys.iter() {
-            if cache_expiry.expiry <= now {
-                self.map.remove(&cache_expiry.key);
-                count += 1;
-            } else {
-                break;
-            }
-        }
+        let now = SteadyTime::now();        
+        let key_count = self.keys.len();
+
+        self.remove_expired_map(now);
         self.keys.retain(|cache_expiry| cache_expiry.expiry > now);
 
         debug_assert!(self.map.len() == self.keys.len(), format!("map.len {:?} != keys.len {:?}", self.map.len(), self.keys.len()));        
-        count
+        key_count - self.keys.len()
+    }
+
+    fn remove_expired_map(&mut self, now: SteadyTime) {
+        //keys are ordered by expiry
+        for cache_expiry in self.keys.iter() {
+            if cache_expiry.expiry > now {
+                break;
+            }
+            debug!("Removing {:?} with expiry {:?}", &cache_expiry.key, cache_expiry.expiry);
+            self.map.remove(&cache_expiry.key);
+        }
     }
 }
 
@@ -153,27 +158,52 @@ mod test {
     use super::{Cache, CacheEntry, CacheKey};
     use std::thread;
     use std::time::Duration;
+    use dns::dns_entities::{DnsAnswer};
 
     fn test_cache() -> Cache {
         let mut cache = Cache::new();
         let key = CacheKey::new(String::from("yahoo.com"), 1, 1);
-        let val = CacheEntry::new(key.clone(), vec![String::from("yahoo.com")], 5);
+        let val = CacheEntry::new(key.clone(), test_answers(), 5);
         cache.upsert(key.clone(), val);
         cache
+    }
+
+    fn test_key_with(name: String) -> CacheKey {
+        CacheKey::new(name, 1, 1)
+    }
+
+    fn test_key() -> CacheKey {
+        test_key_with(String::from("yahoo.com"))
+    }
+
+    fn test_answers() -> Vec<DnsAnswer> {
+        vec![test_answer()]
+    }
+
+    fn test_answers_with(domain: String) -> Vec<DnsAnswer> {
+        vec![test_answer_with(domain)]
+    }
+
+    fn test_answer() -> DnsAnswer {
+        test_answer_with(String::from("yahoo.com"))
+    }
+
+    fn test_answer_with(domain: String) -> DnsAnswer {
+        DnsAnswer::new(domain, 1, 1, 10, 4, vec![200, 200, 200, 200])
     }
 
     #[test]
     fn upsert() {
         let cache = test_cache();
-        let key = CacheKey::new(String::from("yahoo.com"), 1, 1);
-        assert_eq!(cache.get(&key).unwrap().answers[0], String::from("yahoo.com"));
+        let key = test_key();
+        assert_eq!(cache.get(&key).unwrap().answers[0].name, String::from("yahoo.com"));
     }
 
     #[test]
     fn expiry() {
         let mut cache = test_cache();
         let key2 = CacheKey::new(String::from("lycos.com"), 1, 1);
-        let val2 = CacheEntry::new(key2.clone(), vec![String::from("lycos.com")], 100);
+        let val2 = CacheEntry::new(key2.clone(), test_answers_with(String::from("lycos.com")), 100);
         cache.upsert(key2, val2);
 
         assert_eq!(2, cache.len());
